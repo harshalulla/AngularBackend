2af62c5407fe9b329ab87d0c9d850aaf482c60da
Harsha.Lulla <harsha.lulla@ril.com>
2021-01-13 18:54:19 +0530

    updated package name in swaggerConfig

--- InsurancePolicyManagement/src/main/java/com/example/InsurancePolicyManagement/bindings/SwaggerConfig.java
+++ InsurancePolicyManagement/src/main/java/com/example/InsurancePolicyManagement/bindings/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo.bindings"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.InsurancePolicyManagement.bindings"))
                 .paths(regex("/policies.*"))
                 .build();
               
